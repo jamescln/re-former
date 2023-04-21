ff0556b8322099368630ba019dcff0df4b21e05a
James <james.jcarline@gmail.com>
2023-04-21 14:38:01 +0100

    Add strong params to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(allowed_user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def allowed_user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
