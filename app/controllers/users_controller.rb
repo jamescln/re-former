4b17687eee241732bafa1f4bff64e574c833a5f8
jamescln <james.jcarline@gmail.com>
2023-04-25 08:50:55 +0100

    Add edit and show views and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find (params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(allowed_user_params)
+      redirect_to(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def allowed_user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  root "users#new"
+  resources :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
